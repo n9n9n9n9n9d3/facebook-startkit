677840506d2f0f70d8d808288f3eedb1087e1495
n9n9n9n9n9d3 <101703032@nccu.edu.tw>
2015-08-22 17:29:30 +0800

    Update key.js

--- js/key.js
+++ js/key.js
@@ -1,10 +1,11 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
- window.fbAsyncInit = function() {
+<script>
+  window.fbAsyncInit = function() {
     FB.init({
       appId      : '392550567607968',
       xfbml      : true,
-      version    : 'v2.3'
+      version    : 'v2.4'
     });
   };
 
@@ -15,6 +16,7 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 
 
